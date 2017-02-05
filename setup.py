1557948abcf5591d0f59b5908e464725040f4b03
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-02-05 14:13:22 -0800

    Release v0.0.1

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ from setuptools import setup
 
 setup(
     name='dumb-pypi',
-    version='0.0.0',
+    version='0.0.1',
     author='Chris Kuehl',
     author_email='ckuehl@ocf.berkeley.edu',
+    url='https://github.com/chriskuehl/dumb-pypi',
     packages=('dumb_pypi',),
     install_requires=(
         'jinja2',
         'packaging',
     ),
-    classifiers={
+    classifiers=(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    },
+    ),
     entry_points={
         'console_scripts': (
             'dumb-pypi = dumb_pypi.main:main',
