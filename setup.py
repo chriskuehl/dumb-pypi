fdbe2aec165e1653d1ada27701c24766cb09a4ee
Chris Kuehl <ckuehl@yelp.com>
2018-08-23 11:22:02 -0700

    v1.4.0

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='dumb-pypi',
-    version='1.3.1',
+    version='1.4.0',
     author='Chris Kuehl',
-    author_email='ckuehl@ocf.berkeley.edu',
+    author_email='ckuehl@ckuehl.me',
     url='https://github.com/chriskuehl/dumb-pypi',
     packages=['dumb_pypi'],
     install_requires=[
@@ -16,6 +16,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     entry_points={'console_scripts': ['dumb-pypi = dumb_pypi.main:main']},
     package_data={'dumb_pypi': ['templates/*']},
