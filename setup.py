45fc17c95e0a8629b9031f91cc8e543ff3d42338
Anthony Sottile <asottile@umich.edu>
2019-02-28 21:09:03 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,23 +1,2 @@
 from setuptools import setup
-
-
-setup(
-    name='dumb-pypi',
-    version='1.4.0',
-    author='Chris Kuehl',
-    author_email='ckuehl@ckuehl.me',
-    url='https://github.com/chriskuehl/dumb-pypi',
-    packages=['dumb_pypi'],
-    install_requires=[
-        'distlib',
-        'jinja2',
-        'packaging',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    entry_points={'console_scripts': ['dumb-pypi = dumb_pypi.main:main']},
-    package_data={'dumb_pypi': ['templates/*']},
-)
+setup()
